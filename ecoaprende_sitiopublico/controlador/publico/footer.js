2b5de053be449c9902aeaf98930d73a1bb4a8c85
AndersonFigueroa <20220105@ricaldone.edu.sv>
2024-03-21 14:14:20 -0600

    ultima actualizacion

--- ecoaprende_sitiopublico/controlador/publico/footer.js
+++ ecoaprende_sitiopublico/controlador/publico/footer.js
@@ -13,12 +13,12 @@ FOOTER.innerHTML = `
   }
 </style>      
 <div class="container">
-        <div>
-          <p class="fs-6 px-5 text-light" id="pp"><a class="nav-link" href="" target="_blank"> Soporte<br>Tecnico,FAQ<i class="bi bi-facebook fs-4 px-1 text-dark"></i><i class="bi bi-tiktok fs-4 px-1 text-dark"></i><i class="bi bi-envelope-fill fs-4 px-1 text-dark"></i></p>
-        </div>
-        <div>
-          <p class="fs-6 px-5 text-light" id="pp"><a class="nav-link" href="/sport_development/Html/Publico_html/Progreso.html" target="_blank"> <i class="bi bi-c-circle-fill fs-4 px-1 text-dark "></i> EcoAprende, derechos reservados.</p>
-        </div>
+<div>
+<p class="fs-6 px-5 text-light" id="pp"><a class="nav-link" href="" target="_blank"> Soporte Tecnico,FAQ <a href="https://www.facebook.com/profile.php?id=61556163996037"><i class="bi bi-facebook fs-4 px-1 text-dark"></i></a><a href="https://www.tiktok.com/@ecoaprende"><i class="bi bi-tiktok fs-4 px-1 text-dark"></i></a> <i class="bi bi-envelope-fill fs-4 px-1 text-dark"></i></p>
+</div>
+<div>
+<p class="fs-6 px-5 text-light" id="pp"><a class="nav-link" href="" target="_blank"> <i class="bi bi-c-circle-fill fs-4 px-1 text-dark "></i> EcoAprende, derechos reservados.</p>
+</div>
       </div>
     </nav>
     
