066069b25ffb6329c687e1b2dd03ee884d25fb25
Riser245 <20220105@ricaldone.edu.sv>
2024-05-14 20:49:38 -0600

    js

---
+++ ecoaprende_sitioprivado/api_sitioprivado/modelos/handler/pedidos_handler.php
@@ -0,0 +1,115 @@
+<?php
+// Se incluye la clase para trabajar con la base de datos.
+require_once('../../helpers/database.php');
+/*
+*	Clase para manejar el comportamiento de los datos de las tablas PEDIDO y DETALLE_PEDIDO.
+*/
+class PedidoHandler
+{
+    /*
+    *   Declaración de atributos para el manejo de datos.
+    */
+    protected $id_pedido = null;
+    protected $id_detalle = null;
+    protected $cliente = null;
+    protected $producto = null;
+    protected $cantidad = null;
+    protected $estado = null;
+
+    /*
+    *   ESTADOS DEL PEDIDO
+    *   Pendiente (valor por defecto en la base de datos). Pedido en proceso y se puede modificar el detalle.
+    *   Finalizado. Pedido terminado por el cliente y ya no es posible modificar el detalle.
+    *   Entregado. Pedido enviado al cliente.
+    *   Anulado. Pedido cancelado por el cliente después de ser finalizado.
+    */
+
+    /*
+    *   Métodos para realizar las operaciones SCRUD (search, create, read, update, and delete).
+    */
+    // Método para verificar si existe un pedido en proceso con el fin de iniciar o continuar una compra.
+    public function getOrder()
+    {
+        $this->estado = 'Pendiente';
+        $sql = 'SELECT id_pedido
+                FROM pedido
+                WHERE estado_pedido = ? AND id_cliente = ?';
+        $params = array($this->estado, $_SESSION['idCliente']);
+        if ($data = Database::getRow($sql, $params)) {
+            $_SESSION['idPedido'] = $data['id_pedido'];
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // Método para iniciar un pedido en proceso.
+    public function startOrder()
+    {
+        if ($this->getOrder()) {
+            return true;
+        } else {
+            $sql = 'INSERT INTO pedido(direccion_pedido, id_cliente)
+                    VALUES((SELECT direccion_cliente FROM cliente WHERE id_cliente = ?), ?)';
+            $params = array($_SESSION['idCliente'], $_SESSION['idCliente']);
+            // Se obtiene el ultimo valor insertado de la llave primaria en la tabla pedido.
+            if ($_SESSION['idPedido'] = Database::getLastRow($sql, $params)) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+
+    // Método para agregar un producto al carrito de compras.
+    public function createDetail()
+    {
+        // Se realiza una subconsulta para obtener el precio del producto.
+        $sql = 'INSERT INTO detalle_pedido(id_producto, precio_producto, cantidad_producto, id_pedido)
+                VALUES(?, (SELECT precio_producto FROM producto WHERE id_producto = ?), ?, ?)';
+        $params = array($this->producto, $this->producto, $this->cantidad, $_SESSION['idPedido']);
+        return Database::executeRow($sql, $params);
+    }
+
+    // Método para obtener los productos que se encuentran en el carrito de compras.
+    public function readDetail()
+    {
+        $sql = 'SELECT id_detalle, nombre_producto, detalle_pedido.precio_producto, detalle_pedido.cantidad_producto
+                FROM detalle_pedido
+                INNER JOIN pedido USING(id_pedido)
+                INNER JOIN producto USING(id_producto)
+                WHERE id_pedido = ?';
+        $params = array($_SESSION['idPedido']);
+        return Database::getRows($sql, $params);
+    }
+
+    // Método para finalizar un pedido por parte del cliente.
+    public function finishOrder()
+    {
+        $this->estado = 'Finalizado';
+        $sql = 'UPDATE pedido
+                SET estado_pedido = ?
+                WHERE id_pedido = ?';
+        $params = array($this->estado, $_SESSION['idPedido']);
+        return Database::executeRow($sql, $params);
+    }
+
+    // Método para actualizar la cantidad de un producto agregado al carrito de compras.
+    public function updateDetail()
+    {
+        $sql = 'UPDATE detalle_pedido
+                SET cantidad_producto = ?
+                WHERE id_detalle = ? AND id_pedido = ?';
+        $params = array($this->cantidad, $this->id_detalle, $_SESSION['idPedido']);
+        return Database::executeRow($sql, $params);
+    }
+
+    // Método para eliminar un producto que se encuentra en el carrito de compras.
+    public function deleteDetail()
+    {
+        $sql = 'DELETE FROM detalle_pedido
+                WHERE id_detalle = ? AND id_pedido = ?';
+        $params = array($this->id_detalle, $_SESSION['idPedido']);
+        return Database::executeRow($sql, $params);
+    }
+}
---
+++ ecoaprende_sitioprivado/api_sitioprivado/servicios/pedidos.php
@@ -0,0 +1,101 @@
+<?php
+// Se incluye la clase del modelo.
+require_once('../../models/data/pedido_data.php');
+
+// Se comprueba si existe una acción a realizar, de lo contrario se finaliza el script con un mensaje de error.
+if (isset($_GET['action'])) {
+    // Se crea una sesión o se reanuda la actual para poder utilizar variables de sesión en el script.
+    session_start();
+    // Se instancia la clase correspondiente.
+    $pedido = new PedidoData;
+    // Se declara e inicializa un arreglo para guardar el resultado que retorna la API.
+    $result = array('status' => 0, 'session' => 0, 'message' => null, 'error' => null, 'exception' => null, 'dataset' => null);
+    // Se verifica si existe una sesión iniciada como cliente para realizar las acciones correspondientes.
+    if (isset($_SESSION['idCliente'])) {
+        $result['session'] = 1;
+        // Se compara la acción a realizar cuando un cliente ha iniciado sesión.
+        switch ($_GET['action']) {
+            // Acción para agregar un producto al carrito de compras.
+            case 'createDetail':
+                $_POST = Validator::validateForm($_POST);
+                if (!$pedido->startOrder()) {
+                    $result['error'] = 'Ocurrió un problema al iniciar el pedido';
+                } elseif (
+                    !$pedido->setProducto($_POST['idProducto']) or
+                    !$pedido->setCantidad($_POST['cantidadProducto'])
+                ) {
+                    $result['error'] = $pedido->getDataError();
+                } elseif ($pedido->createDetail()) {
+                    $result['status'] = 1;
+                    $result['message'] = 'Producto agregado correctamente';
+                } else {
+                    $result['error'] = 'Ocurrió un problema al agregar el producto';
+                }
+                break;
+            // Acción para obtener los productos agregados en el carrito de compras.
+            case 'readDetail':
+                if (!$pedido->getOrder()) {
+                    $result['error'] = 'No ha agregado productos al carrito';
+                } elseif ($result['dataset'] = $pedido->readDetail()) {
+                    $result['status'] = 1;
+                } else {
+                    $result['error'] = 'No existen productos en el carrito';
+                }
+                break;
+            // Acción para actualizar la cantidad de un producto en el carrito de compras.
+            case 'updateDetail':
+                $_POST = Validator::validateForm($_POST);
+                if (
+                    !$pedido->setIdDetalle($_POST['idDetalle']) or
+                    !$pedido->setCantidad($_POST['cantidadProducto'])
+                ) {
+                    $result['error'] = $pedido->getDataError();
+                } elseif ($pedido->updateDetail()) {
+                    $result['status'] = 1;
+                    $result['message'] = 'Cantidad modificada correctamente';
+                } else {
+                    $result['error'] = 'Ocurrió un problema al modificar la cantidad';
+                }
+                break;
+            // Acción para remover un producto del carrito de compras.
+            case 'deleteDetail':
+                if (!$pedido->setIdDetalle($_POST['idDetalle'])) {
+                    $result['error'] = $pedido->getDataError();
+                } elseif ($pedido->deleteDetail()) {
+                    $result['status'] = 1;
+                    $result['message'] = 'Producto removido correctamente';
+                } else {
+                    $result['error'] = 'Ocurrió un problema al remover el producto';
+                }
+                break;
+            // Acción para finalizar el carrito de compras.
+            case 'finishOrder':
+                if ($pedido->finishOrder()) {
+                    $result['status'] = 1;
+                    $result['message'] = 'Pedido finalizado correctamente';
+                } else {
+                    $result['error'] = 'Ocurrió un problema al finalizar el pedido';
+                }
+                break;
+            default:
+                $result['error'] = 'Acción no disponible dentro de la sesión';
+        }
+    } else {
+        // Se compara la acción a realizar cuando un cliente no ha iniciado sesión.
+        switch ($_GET['action']) {
+            case 'createDetail':
+                $result['error'] = 'Debe iniciar sesión para agregar el producto al carrito';
+                break;
+            default:
+                $result['error'] = 'Acción no disponible fuera de la sesión';
+        }
+    }
+    // Se obtiene la excepción del servidor de base de datos por si ocurrió un problema.
+    $result['exception'] = Database::getException();
+    // Se indica el tipo de contenido a mostrar y su respectivo conjunto de caracteres.
+    header('Content-type: application/json; charset=utf-8');
+    // Se imprime el resultado en formato JSON y se retorna al controlador.
+    print(json_encode($result));
+} else {
+    print(json_encode('Recurso no disponible'));
+}
