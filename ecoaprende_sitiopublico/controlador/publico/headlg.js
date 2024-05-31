63f759b14a54f7f35aff48467c91b88020026ec7
AnderFigueroa <20220105@ricaldone.edu.sv>
2024-05-31 13:59:19 -0600

    header login

---
+++ ecoaprende_sitiopublico/controlador/publico/headlg.js
@@ -0,0 +1,51 @@
+const HEADERLG = document.querySelector('header');
+HEADERLG.innerHTML = `
+
+<nav class="navbar">
+  <div class="container">
+    
+  </div>
+</nav>
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+  <img src="/ecoaprende_sitiopublico/recursos/img/lg.png" alt="lg" width="100" height="55"  >
+  </a>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item  ">
+        </li>
+        <li class="nav-item  ">
+        </li>
+      </ul>
+      <span class="navbar-text me-4">
+      <a class="nav-link justify-content-end ms-auto active" aria-current="page">¿No tienes cuenta aun?</a>
+      </span>
+      <span class="navbar-text me-4">
+      <button type="button justify-content-end " class="btn">Inicia aqui</button>
+      </span>
+    </div>
+  </div>
+</nav>
+<!-- contralador de el header -->
+<style>
+  .navbar {
+    background-color: rgba(119, 127, 71, 1) !important;
+  }
+  li {
+    font-family: 'Ropa Sans';
+  }
+  .custom-color{
+      color: rgba(119, 127, 71, 1) !important;
+  }
+  .btn{
+    background-color: rgba(39, 59, 9, 1);
+    color: white;
+  }
+  .navbar-nav{
+    text-align: rigth;
+    justify-contend:end;
+  }
+</style>
+
+    
+`
