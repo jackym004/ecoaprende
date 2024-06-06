303c10afdce405c2d9194cebeea39f213d6f3e3e
JoseCastillo2023 <20220235@ricaldone.edu.sv>
2024-06-06 15:32:25 -0600

    Arreglo de registar cuenta

--- ecoaprende_sitiopublico/controlador/publico/footer_index.js
+++ ecoaprende_sitiopublico/controlador/publico/footer_index.js
@@ -1,25 +1,35 @@
 const FOOTER = document.querySelector('footer');
 FOOTER.innerHTML = `
 
-<nav class="navbar fixed-bottom  bg-body-tertiary border-bottom border-body mt-5" data-bs-theme="dark">
-<style>
-  .navbar {
-    background-color: rgba(119, 127, 71, 1)
-    !important;a
-  }
-
-  p{
-    font-family:'Ropa Sans';
-  }
-</style>      
-<div class="container">
-        <div>
-          <p class="fs-6 px-5 text-light" id="pp"><a class="nav-link" href="" target="_blank"> Soporte<br>Tecnico,FAQ<i class="bi bi-facebook fs-4 px-1 text-dark"></i><i class="bi bi-tiktok fs-4 px-1 text-dark"></i><i class="bi bi-envelope-fill fs-4 px-1 text-dark"></i></p>
+<footer>
+<div class="footer container-fluid sticky-bottom">
+    <div class="container py-3">
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-4 col-md-4 col-lg-4 m x-auto">
+                <p class="text-white my-4">
+                    <i class="bi bi-c-circle-fill text-black"></i> Copyright 2024 Ecoaprende
+                </p>
+            </div>
+            <div class="col-sm-4 col-md-4 col-lg-4 mx-auto">
+                <img class="img-fluid" src="../../recursos/img/Sitio_Público__1_-removebg-preview.png"
+                    alt="Logo">
+            </div>
+            <div class="col-sm-4 col-md-4 col-lg-4 mx-auto">
+                <div class="row mx-auto">
+                    <a href="https://www.facebook.com/"><p class="text-white mx-5">Facebook</p></a>
+                </div>
+                <div class="row mx-auto">
+                    <a href="https://www.tiktok.com/"><p class="text-white mx-5">Tiktok</p></a>
+                </div>
+                <div class="row mx-auto">
+                    <a href="https://www.google.com/intl/es/gmail/about/"><p class="text-white mx-5">Correo electronico</p></a>
+                </div>
+            </div>
         </div>
-        <div>
-          <p class="fs-6 px-5 text-light" id="pp"><a class="nav-link" href="/sport_development/Html/Publico_html/Progreso.html" target="_blank"> <i class="bi bi-c-circle-fill fs-4 px-1 text-dark "></i> EcoAprende, derechos reservados.</p>
-        </div>
-      </div>
-    </nav>
-    
+    </div>
+    <div class="py-3"></div>
+</div>
+</footer> 
 `
--- ecoaprende_sitiopublico/controlador/publico/headlg.js
+++ ecoaprende_sitiopublico/controlador/publico/headlg.js
@@ -1,11 +1,6 @@
 const HEADERLG = document.querySelector('header');
 HEADERLG.innerHTML = `
 
-<nav class="navbar">
-  <div class="container">
-    
-  </div>
-</nav>
 <nav class="navbar sticky-top navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
   <img src="../../recursos/img/lg.png" alt="lg" width="100" height="55"  >
@@ -21,7 +16,7 @@ HEADERLG.innerHTML = `
       <a class="nav-link justify-content-end ms-auto active no" aria-current="page">¿No tienes cuenta aun?</a>
       </span>
       <span class="navbar-text me-4">
-      <a type="button justify-content-end" href="../../vistas/publica/Registro.html" class="btn regist text-white">Inicia aqui</a>
+      <a type="button justify-content-end" href="../../vistas/publica/registro.html" class="btn regist text-white">Inicia aqui</a>
       </span>
     </div>
   </div>
