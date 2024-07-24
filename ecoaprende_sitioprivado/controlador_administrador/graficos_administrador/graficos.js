4d7ceb06eae202b97f3fe50ad0e7be123cd78e78
AnderFigueroa <20220105@ricaldone.edu.sv>
2024-07-23 22:16:25 -0600

    arreglos 2.0

--- ecoaprende_sitioprivado/controlador_administrador/graficos_administrador/graficos.js
+++ ecoaprende_sitioprivado/controlador_administrador/graficos_administrador/graficos.js
@@ -3,24 +3,22 @@ const LIBROS_API = 'servicios/administrador/libros.php';
 
 // Método del evento para cuando el documento ha cargado.
 document.addEventListener('DOMContentLoaded', () => {
-    // Constante para obtener el número de horas.
-    const HOUR = new Date().getHours();
-    // Se define una variable para guardar un saludo.
-    let greeting = '';
-    // Dependiendo del número de horas transcurridas en el día, se asigna un saludo para el usuario.
-    if (HOUR < 12) {
-        greeting = 'Buenos días';
-    } else if (HOUR < 19) {
-        greeting = 'Buenas tardes';
-    } else if (HOUR <= 23) {
-        greeting = 'Buenas noches';
-    }
-    // Llamada a la función para mostrar el encabezado y pie del documento.
-    // Llamada a la funciones que generan los gráficos en la página web.
-    graficoBarrasCategorias();
-    graficoPastelCategorias();
+  // Constante para obtener el número de horas.
+  const HOUR = new Date().getHours();
+  // Se define una variable para guardar un saludo.
+  let greeting = '';
+  // Dependiendo del número de horas transcurridas en el día, se asigna un saludo para el usuario.
+  if (HOUR < 12) {
+      greeting = 'Buenos días';
+  } else if (HOUR < 19) {
+      greeting = 'Buenas tardes';
+  } else if (HOUR <= 23) {
+      greeting = 'Buenas noches';
+  }
+  // Llamada a la funciones que generan los gráficos en la página web.
+  graficoBarrasCategorias();
+  graficoPastelCategorias();
 });
-
 /*
 *   Función asíncrona para mostrar un gráfico de barras con la cantidad de productos por categoría.
 *   Parámetros: ninguno.
@@ -41,7 +39,7 @@ const graficoBarrasCategorias = async () => {
             cantidades.push(row.cantidad);
         });
         // Llamada a la función para generar y mostrar un gráfico de barras. Se encuentra en el archivo components.js
-        barGraph('chart1', categorias, cantidades, 'Cantidad de productos', 'Cantidad de productos por categoría');
+        barGraph('chart1', categorias, cantidades, 'Cantidad de libros', 'Cantidad de libros por categoría');
     } else {
         document.getElementById('chart1').remove();
         console.log(DATA.error);
@@ -68,7 +66,7 @@ const graficoPastelCategorias = async () => {
             porcentajes.push(row.porcentaje);
         });
         // Llamada a la función para generar y mostrar un gráfico de pastel. Se encuentra en el archivo components.js
-        pieGraph('chart2', categorias, porcentajes, 'Porcentaje de productos por categoría');
+        pieGraph('chart2', categorias, porcentajes, 'Porcentaje de libros por categoría');
     } else {
         document.getElementById('chart2').remove();
         console.log(DATA.error);
